5325d6f3e6122cc0826ce3ec11703ee9932e65ab
wwang33 <wwang33@ncsu.edu>
2020-12-23 13:40:09 -0800

    added variables, sprites, sprite, and some test cases

---
+++ src/variables.js
@@ -0,0 +1,30 @@
+class Variables {
+
+    constructor (snapAdapter) {
+        /**
+         * @type{SnapAdapter}
+         */
+        this.snapAdapter = snapAdapter;
+
+    }
+
+    get stageVariables () {
+        return  this.snapAdapter.ide.globalVariables.vars;
+    }
+
+    getFirstVariableValue(){
+        let allValues = Object.keys(this.stageVariables)
+            .map(v => ({
+                name: v,
+                value: stageVariables[v].value
+            }));
+        return allValues[0]
+    }
+
+
+
+
+
+
+}
+module.exports = Variables;
